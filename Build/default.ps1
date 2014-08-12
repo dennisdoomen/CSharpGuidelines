3b39200a696c0c5e6d30cb2e5aa4341b4b6b350e
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2014-08-12 17:16:24 +0200

    Added a PSake script to compile the sections in the final Markdown file.

---
+++ Build/default.ps1
@@ -0,0 +1,26 @@
+﻿properties {
+    $BaseDirectory = Resolve-Path ..     
+}
+
+task default -depends Clean, Compile
+
+task Clean {	
+    TeamCity-Block "Removing build output from previous runs" {
+		Get-ChildItem $BaseDirectory\Guidelines CSharp*.md | ForEach { Remove-Item $_.FullName }
+    }
+}
+
+task Compile {
+    TeamCity-Block "Compiling Markdown file from individual section" {
+		$files = (dir $BaseDirectory\Guidelines\*.md)
+		$outfile = "$BaseDirectory\Guidelines\CSharpCodingGuidelines.md"
+
+		$files | %{
+			Write-Host "Including " + $_.FullName
+		    Get-Content $_.FullName | Add-Content $outfile
+		}
+		
+		Write-Host "Opening the output file using its default program"
+		& $outfile
+    }
+}
